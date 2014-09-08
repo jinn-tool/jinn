631af96b03767b0b22f1b6d0fc2ff5bd4e5b4f89
Richard Walker <digitalsadhu@gmail.com>
2014-09-08 23:55:47 +0200

    Add chai assertion library to test setup

---
+++ assets/test/example.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var should = require('chai').should()
+
+describe('Maths', () => {
+  describe('adding 2 numbers', () => {
+    it('should total correctly', () => {
+      var result = 1 + 2
+      result.should.equal(3)
+    })
+  })
+})
