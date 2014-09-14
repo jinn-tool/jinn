25997d933513f8d0bbcb95390e71be532b1e095f
Richard Walker <digitalsadhu@gmail.com>
2014-09-15 01:02:51 +0200

    Refactor to use modules for various tasks

--- assets/base/index.js
+++ assets/base/index.js
@@ -1,7 +1,4 @@
 'use strict';
 
-//include env vars from .env file
-require('envoodoo')()
-
 //bootstrap your app
 // eg. require('lib/app.js')
--- index.js
+++ index.js
@@ -2,59 +2,101 @@
 
 'use strict';
 
+//dependencies
 var program = require('commander')
   , gulp    = require('gulp')
   , chalk   = require('chalk')
   , exec    = require('exec')
   , pjson   = require('./package.json')
 
+//modules
+var jshintPlugin       = require('jinn-jshint')
+  , gitignorePlugin    = require('jinn-gitignore')
+  , envPlugin          = require('jinn-env')
+  , editorconfigPlugin = require('jinn-editorconfig')
+  , testPlugin         = require('jinn-test')
+
+//magic strings
 var messages = {
   create: 'Creating new project',
   install: 'Installing dependencies',
-  complete: 'Done!',
+  complete: 'Project created!',
   esnext: 'including esnext'
 }
 
+//paths
 var sources = {
   base: __dirname + '/assets/base/**/*',
   esnext: __dirname + '/assets/esnext/**/*'
-  // gitignore: __dirname + '/assets/.gitignore'
 }
 
 function notify(message) {
-  if (!program.quiet)
-    console.log(chalk.green(message))
+  console.log(chalk.green('-->',message))
 }
 
-function installDependencies(name) {
+function installDependencies(appname) {
   notify(messages.install)
 
-  exec('cd ' + name + ' && npm install', function () {
+  exec('cd ' + appname + ' && npm install', function () {
     notify(messages.complete)
+    process.exit(0)
   })
 }
 
-function newProject(name) {
-  notify(messages.create)
-  var paths = [sources.base]
-
-  if (program.esnext) {
-    notify(messages.esnext)
-    paths.push(sources.esnext)
+function installModules(appname) {
+  var jinnObject = {
+    appname: appname,
+    log: notify,
+    options: program
   }
 
-  gulp.src(paths, {dot: true})
+  jshintPlugin(jinnObject, function () {
+    gitignorePlugin(jinnObject, function () {
+      envPlugin(jinnObject, function() {
+        editorconfigPlugin(jinnObject, function() {
+          testPlugin(jinnObject, function() {
+            installDependencies(appname)
+          })
+        })
+      })
+    })
+  })
+}
+
+function newProject(appname) {
+  console.log(chalk.green(messages.create))
+
+  gulp.src([sources.base], {dot: true})
     .pipe(gulp.dest(process.cwd() + '/' + name))
-    .on('end', installDependencies.bind(this, name))
+    .on('end', function () {
+      installModules(appname)
+    })
 }
 
 program
   .version(pjson.version)
-  .option('-q, --quiet', 'Hide logging information')
   .option('-e, --esnext', 'Add esnext support through the traceur compiler')
+  .option('-h, --heroku', '')
+  .option('-b, --browserify', '')
+  .option('-c, --coffee', '')
+  .option('-i, --iced', '')
+  .option('-j, --jshint', '')
+  .option('-x, --circle', '')
+  .option('-t, --travis', '')
+  .option('-o, --coverage', '')
+  .option('-a, --coveralls', '')
+  .option('-g, --github', '')
+  .option('-l, --license', '')
+  .option('-E, --env')
+  .option('-d, --editorconfig', '')
+  .option('-r, --readme', '')
+  .option('-u, --test', '')
+  .option('-n, --client', '')
+  .option('-s, --server', '')
+  .option('-p, --npm', '')
 
 program
-  .command('new <name>')
+  .command('new <appname>')
   .description('Scaffold out a new app with given name')
   .action(newProject)
 
